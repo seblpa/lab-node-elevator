e9ffcb41379e341fccf647d29e2a41c6c717dbd5
Sébastien Del Mestre <sebastien.delmestre@gmail.com>
2018-01-30 07:46:15 +0100

    update and passengerEnter functions done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,68 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "up";
+    this.interval;
+    this.waitingList = [];
+    this.passengers = [];
+  }
+  
+  start() { 
+    this.interval = setInterval(() => {
+      this.update();
+      this.floorUp();
+    }, 1000);
+    
+  }
+  
+  stop() {
+    clearInterval(this.interval);
+  }
+  
+  update(person) { 
+    this.log();
+    this._passengersEnter(person);
+    this._passengerLeave(person);
+    this.stop();
+   }
+
+  _passengersEnter(person) { 
+    for (var i = 0; i < this.waitingList.length; i++){
+      if(this.waitingList[i].originFloor == this.floor) {
+        this.passengers.push(person);
+        this.waitingList.pop(person);
+        this.requests.push(person.destinationFloor)
+      console.log(person.name + 'has entered on' + this.floor)
+      } 
+    }
+  }
+  _passengersLeave(person) { }
+
+  floorUp() { 
+    if (this.floor < this.MAXFLOOR && this.direction == "up"){
+      this.floor++;
+    }  else {
+       this.direction = "down";
+       this.floorDown();
+      }
+    }
+  
+  floorDown() { 
+    if (this.floor > 0 && this.direction == "down") {
+      this.floor--;
+    } else {
+      this.direction = "up";
+      this.floorUp();
+    }
+  } 
+  call(person) { 
+    this.waitingList.push(person);
+    this.requests.push(person);
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  log() { 
+    console.log('current status is' + this.floor + 'current direction' + this.direction);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,12 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const myElevator = new Elevator();
+const myPerson = new Person("pepe",2,6);
+
+console.log(myElevator)
+
+myElevator.call(myPerson);
+console.log(myElevator);
+
+myElevator.start();
